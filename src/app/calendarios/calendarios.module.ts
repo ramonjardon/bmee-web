2614d590a2ffcc8dbed86551bf8eb386deb8a735
Ramón Jardón Álvarez <ramonjardon@gmail.com>
2022-05-13 22:30:18 +0200

    Subida inicial

---
+++ src/app/calendarios/calendarios.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalendariosRoutingModule } from './calendarios-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CalendariosRoutingModule
+  ]
+})
+export class CalendariosModule { }
